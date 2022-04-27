27603468de6850ac430fd4f7f4fc4802c9e846f6
Janbacer <101245670+Janbacer@users.noreply.github.com>
2022-04-27 09:00:44 +0200

    Físiques personatge millordes

--- Escenes/Personatge.gd
+++ Escenes/Personatge.gd
@@ -3,20 +3,22 @@ extends KinematicBody2D
 var velocitat_base = 250
 var velocitat = Vector2.ZERO 
 var gravetat = Vector2.DOWN * 1000
-var salt = Vector2.UP * 50
+var acceleracio = Vector2.RIGHT * 50
+var salt = Vector2.UP * 35
 var pantalla = 1
 
 func _ready():
-	velocitat.y = 0
+	velocitat.x = 1
 	
 func _physics_process(delta):
-	velocitat.x = 200
+	
 	
 	if Input.is_action_pressed("Vola"):
 		velocitat.y += salt.y
 		
+	if position.x > 1000:
+		velocitat.x = velocitat.x * -1 
 		
+	velocitat += acceleracio * delta
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
-	
-	
