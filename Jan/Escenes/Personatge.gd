4f20ddd7b6b19a0d589d36af5b6f826b35af1571
Janbacer <101245670+Janbacer@users.noreply.github.com>
2022-05-02 12:13:43 +0200

    Carpetes creades

--- Jan/Escenes/Personatge.gd
+++ Jan/Escenes/Personatge.gd
@@ -1,24 +1,31 @@
 extends KinematicBody2D
 
-var velocitat_base = 250
 var velocitat = Vector2.ZERO 
-var gravetat = Vector2.DOWN * 1000
-var acceleracio = Vector2.RIGHT * 50
+var gravetat = Vector2.DOWN * 1200
 var salt = Vector2.UP * 35
 var pantalla = 1
 
 func _ready():
 	velocitat.x = 1
+	position = Vector2(480,50)
 	
 func _physics_process(delta):
-	
-	
 	if Input.is_action_pressed("Vola"):
 		velocitat.y += salt.y
 		
 	if position.x > 1000:
 		velocitat.x = velocitat.x * -1 
 		
-	velocitat += acceleracio * delta
 	velocitat += gravetat * delta
 	velocitat = move_and_slide(velocitat, Vector2.UP)
+
+	anima(velocitat)
+	
+func anima(velocitat: Vector2):
+	var animacio: AnimatedSprite = $AnimatedSprite
+	if Input.is_action_just_pressed("Vola"):
+		animacio.play("volar")
+	if Input.is_action_just_released("Vola"):
+		animacio.play("quiet")
+	if velocitat.y == 0:
+		animacio.play("correr")
