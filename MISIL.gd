605169e8e0a45075816c9e09cebad049d398bb82
luisssdiaz <101245820+luisssdiaz@users.noreply.github.com>
2022-05-25 08:59:54 +0200

    aqfwqwfd

--- MISIL.gd
+++ MISIL.gd
@@ -1,18 +1,16 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var velocidad = -300
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += Vector2(-4, 0)
-	if position.x <= 0:
-		position.x = 1600
+	global_translate(Vector2.RIGHT * velocidad * delta)
+	if global_position.x <= -404:
+		queue_free()
+
+
+func _cuerpo_entro(body):
+	if body is RigidBody2D:
+#		en_juego = false
+		pass
